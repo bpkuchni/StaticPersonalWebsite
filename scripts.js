a0c40760a47265f1d7e88311796a4a88c5462801
Brandon Kuchnicki <bpkuchni@gmail.com>
2024-03-20 09:27:24 -0400

    add a quick burger menu snippet, then stop getting distracted

--- scripts.js
+++ scripts.js
@@ -82,3 +82,12 @@ function toggleContactMe() {
     contact.textContent = "_Me";
   }
 }
+
+function burgerMenuToggle() {
+  var x = document.getElementById("burgerMenu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
