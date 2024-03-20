24cace81498c92d70168f844ad963abc9523caa9
Brandon Kuchnicki <bpkuchni@gmail.com>
2024-03-20 13:55:32 -0400

    try resolving with onload hook

--- scripts.js
+++ scripts.js
@@ -15,6 +15,9 @@ const heroAnchor = document.getElementById("hero-anchor");
 navbar.style.top = getTop() + "px";
 navbar.style.position = "fixed";
 
+const heroImage = document.getElementById("heroImage");
+heroImage.onload = scrollHandler;
+
 function scrollHandler() {
   const element = document.getElementById("nav-container");
   element.style.top = getTop() + "px";
